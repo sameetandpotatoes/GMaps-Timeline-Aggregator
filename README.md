0d04b0bf0195be8c5da9bec6a7b02168bb796f1c
Sameet Sapra <sameetandpotatoes@gmail.com>
2019-06-05 17:18:49 -0500

    Stream an uploaded JSON file and map dates to lat/lon points

--- README.md
+++ README.md
@@ -1,68 +1,9 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Google Maps Timeline Aggregator
 
-## Available Scripts
+## Motivation
 
-In the project directory, you can run:
+The Timeline feature in Google Maps is a pretty interesting way to track your whereabouts throughout a day.
 
-### `npm start`
+For me, I especially like to check it out whenever I am walking around a lot to see how much ground in a city I have covered. One thing that would be even more interesting to see is that data aggregated over a certain timeperiod, so you can see how much of a city you have visited in a given time.
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+Google Maps' Timeline doesn't have this feature, so I thought it might be neat to build a quick visualizer for this.
--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
 import React from 'react';
+import oboe from 'oboe';
+import moment from 'moment';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  init() {
+    this.parseJSONFile = this.parseJSONFile.bind(this)
+  }
+
+  parseJSONFile(file) {
+    var fileSize = file.size;
+    console.log("File Size: " + fileSize);
+    // var prettyFileSize = prettySize(fileSize);
+    // TODO update MB state
+
+		var chunkSize = 512 * 1024; // bytes
+		var offset = 0;
+		var self = this; // we need a reference to the current object
+		var chunkReaderBlock = null;
+    
+    var startTime = Date.now();
+    var endTime = Date.now();
+    var SCALAR_E7 = 0.0000001; // Since Google Takeout stores latlngs as integers
+    
+    function prettyLatLon(lat) {
+      return lat * SCALAR_E7;
+    }
+
+    var datePointMap = {};
+
+    var oboeInstance = new oboe();
+    oboeInstance
+      .node('locations.*', function(location) {
+        var dateStr = moment(new Date(parseInt(location.timestampMs))).format("MM/DD/YYYY");
+        var locEntry = {
+          timestamp: parseInt(location.timestampMs),
+          lat: prettyLatLon(location.latitudeE7), 
+          lon: prettyLatLon(location.longitudeE7)
+        };
+        if (datePointMap.hasOwnProperty(dateStr)) {
+          datePointMap[dateStr].push(locEntry);
+        } else {
+          datePointMap[dateStr] = [locEntry];
+        }
+      }).done(function() {
+        console.log(datePointMap);
+        // display the range of dates (get min / max) and then upon selecting, it should just plot the points for that
+      });
+    
+    var readEventHandler = function ( evt ) {
+      if ( evt.target.error == null ) {
+        offset += evt.target.result.length;
+        var chunk = evt.target.result;
+        var percentLoaded = ( 100 * offset / fileSize ).toFixed( 0 );
+        console.log(percentLoaded);
+        // status( percentLoaded + '% of ' + prettyFileSize + ' loaded...' );
+        oboeInstance.emit( 'data', chunk ); // callback for handling read chunk
+      } else {
+        return;
+      }
+      if ( offset >= fileSize ) {
+        oboeInstance.emit( 'done' );
+        return;
+      }
+
+      // of to the next chunk
+      chunkReaderBlock( offset, chunkSize, file );
+    }
+
+    chunkReaderBlock = function ( _offset, length, _file ) {
+      var r = new FileReader();
+      var blob = _file.slice( _offset, length + _offset );
+      r.onload = readEventHandler;
+      r.readAsText( blob );
+    }
+
+    // now let's start the read with the first block
+    chunkReaderBlock( offset, chunkSize, file );
+  }
+  
+  onChangeHandler(e) {
+    console.log(e.target.files[0])
+    this.parseJSONFile(e.target.files[0])
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>       
+          <input type="file" name="file" onChange={this.onChangeHandler.bind(this)}/>
+        </header>
+      </div>
+    );
+  }  
 }
 
 export default App;
